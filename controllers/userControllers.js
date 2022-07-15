4289cc46789be0b3ab13f7ef9a9b2958caf28b19
francisco.favaro <ffavaro@escrbianos.org>
2022-07-14 22:33:39 -0300

    nuevo usuario

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,33 @@
+const path = require("path");
+const fs = require("fs");
+const bcrypt = require('bcrypt');
+
+const usersFilePath = path.join(
+  __dirname,
+  "../src/data/users.json"
+);
+const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8")); // Convierte el JSON en un array de objetos literales
+
 let controller = {
     register: function (req, res) {
         res.render('./users/register');
+    },
+    store: (req, res) => {
+        console.log(req.file);
+        let hash =  bcrypt.hashSync(req.body.password, 10);
+        let newUser = {
+            id: req.body.id,
+            firstname: req.body.name,
+            lastname: req.body.description,
+            email: req.body.email,
+            password:hash,
+            category: req.body.category,
+            image: req.file.filename
+          };
+          users.push(newUser);
+          let jsonProduct = JSON.stringify(users);
+          fs.writeFileSync(usersFilePath, jsonProduct); //Reemplaza el archivo JSON anterior por el nuevo producto
+          res.redirect('/');
     }
 }
 
--- routers/user.js
+++ routers/user.js
@@ -2,6 +2,21 @@ const express = require('express');
 const router = express.Router();
 const userControllers = require('../controllers/userControllers');
 
-router.get('/register', userControllers.register);
+const path = require('path')
+//multer
+const multer =require('multer');
 
+var storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'public/images/users')
+	},
+	filename: (req, file, cb) =>{
+		cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+	}
+})
+
+const uploadFile = multer({ storage: storage })
+
+router.get('/register', userControllers.register);
+router.post('/new', uploadFile.single('avatar'), userControllers.store)
 module.exports = router;
